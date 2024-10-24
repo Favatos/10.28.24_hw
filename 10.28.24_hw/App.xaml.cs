eb02d10aaaa31bfa0334ea54d5ec2ab7961867a8
werty werty <werty36401@gmail.com>
2024-10-24 12:38:18 +0300

    Добавьте файлы проекта.

---
+++ 10.28.24_hw/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _10._28._24_hw
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
